904eb6311c8340b5c2a974db0d219ae2937ea41c
tonsu <tonsu>
2026-02-11 17:44:04 +0700

    Add HTTPS support for local dev with vite basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    basicSsl(),
   ],
 })
